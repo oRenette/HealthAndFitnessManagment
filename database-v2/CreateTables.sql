81da5b080aeddbde3d66d8cc545d252c1faf06f7
NolanKisser123 <146011789+NolanKisser123@users.noreply.github.com>
2024-04-08 18:15:23 -0400

    Fixed Issues when creating CreateTables.sql
    Added Class_members and Class_Exercises Tables, also changed to Classes to avoid having to write in "". Added member_class_bookings and admin_class_bookings to allow for room booking management.

--- database-v2/CreateTables.sql
+++ database-v2/CreateTables.sql
@@ -1,8 +1,12 @@
 CREATE DATABASE HealthAndFitness;
 
-CREATE TYPE week_day AS enum (
-  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-)
+CREATE TYPE week_day AS enum ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+CREATE TYPE Booking_Status AS ENUM ('Booked', 'Cancelled', 'Modified');
+
+CREATE TABLE Exercises (
+  exercise_id SERIAL PRIMARY KEY,
+  exercise_name varchar(255)
+);
 
 CREATE TABLE Profile (
   profile_id SERIAL PRIMARY KEY,
@@ -15,15 +19,17 @@ CREATE TABLE Trainers (
   trainer_id SERIAL PRIMARY KEY,
   first_name varchar(255),
   last_name varchar(255),
+  email varchar(255),
   day_schedule week_day ARRAY[7],
   start_time time ARRAY[7],
-  end_time time ARRAY[7],
+  end_time time ARRAY[7]
 );
 
 CREATE TABLE AdminStaff (
   admin_id SERIAL PRIMARY KEY,
   first_name varchar(255),
-  last_name varchar(255)
+  last_name varchar(255),
+  email varchar(255)
 );
 
 CREATE TABLE HealthStatistics (
@@ -47,7 +53,7 @@ CREATE TABLE Billings (
   bill_id SERIAL PRIMARY KEY,
   member_id int REFERENCES Members(member_id),
   admin_id int REFERENCES AdminStaff(admin_id),
-  amount double
+  amount double precision
 );
 
 CREATE TABLE Rooms (
@@ -56,36 +62,49 @@ CREATE TABLE Rooms (
   capacity int
 );
 
-CREATE TABLE Bookings (
-  booking_id int PRIMARY KEY,
-  admin_id int REFERENCES AdminStaff(admin_id),
-  room_ids int ARRAY REFERENCES Rooms(room_id),
-  member_ids int ARRAY REFERENCES Members(member_id),
-  trainer_ids int ARRAY REFERENCES Trainers(trainer_id)
-);
-
-CREATE TABLE FitnessGoals (
-  goal_id int PRIMARY KEY,
-  member_id int REFERENCES Members(member_id),
-  weight int,
-  time_deadline date
-);
-
-CREATE TABLE Exercises (
-  exercise_id int PRIMARY KEY,
-  exercise_name varchar(255)
-);
-
-CREATE TABLE "Class" (
-  class_id int PRIMARY KEY,
+CREATE TABLE Classes (
+  class_id SERIAL PRIMARY KEY,
   trainer_id int REFERENCES Trainers(trainer_id),
   room_id int REFERENCES Rooms(room_id),
   is_group boolean,
-  member_ids int ARRAY REFERENCES Members(member_id),
-  exercise_ids int ARRAY REFERENCES Exercises(exercise_id),
-  day_schedule week_day ARRAY[7],
-  start_time time ARRAY[7],
-  end_time time ARRAY[7],
+  day_schedule week_day,
+  start_time time,
+  end_time time,
   creation_time timestamp,
-  price double
+  price double precision
+);
+
+CREATE TABLE Class_Members (
+  class_id int REFERENCES Classes(class_id),
+  member_id int REFERENCES Members(member_id),
+  PRIMARY KEY (class_id, member_id)
+);
+
+CREATE TABLE Class_Exercises (
+  class_id int REFERENCES Classes(class_id),
+  exercise_id int REFERENCES Exercises(exercise_id),
+  PRIMARY KEY (class_id, exercise_id)
+);
+
+CREATE TABLE Member_Class_Bookings (
+  booking_id SERIAL PRIMARY KEY,
+  member_id INT REFERENCES Members(member_id),
+  class_id INT REFERENCES Classes(class_id),
+  booking_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  status Booking_Status DEFAULT 'Booked'
+);
+
+CREATE TABLE Admin_Class_Bookings (
+  booking_id INT REFERENCES Member_Class_Bookings(booking_id) ON DELETE CASCADE,
+  admin_id INT REFERENCES AdminStaff(admin_id),
+  modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (booking_id, admin_id)
+);
+
+
+CREATE TABLE FitnessGoals (
+  goal_id SERIAL PRIMARY KEY,
+  member_id int REFERENCES Members(member_id),
+  weight int,
+  time_deadline date
 );
