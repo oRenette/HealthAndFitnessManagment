c887c5e4e6b339920db48ba7763af2b2e8154e25
NolanKisser123 <146011789+NolanKisser123@users.noreply.github.com>
2024-04-12 15:18:10 -0400

    Update dml.sql

--- database-v2/dml.sql
+++ database-v2/dml.sql
@@ -31,9 +31,9 @@ INSERT INTO HealthStatistics (weight, age, male, height) VALUES
 
 -- Inserting data into Members table
 INSERT INTO Members (profile_id, health_id) Values 
-(1,1),
-(2,2),
-(3,3);
+(1, 1),
+(2, 2),
+(3, 3);
 
 -- Inserting data into Billings table
 INSERT INTO Billings (member_id, admin_id, amount) VALUES
@@ -82,3 +82,10 @@ INSERT INTO FitnessGoals (member_id, weight, time_deadline) VALUES
 (1, 65, '2024-06-30'),
 (2, 60, '2024-07-15'),
 (3, 75, '2024-05-20');
+
+-- Inserting data into Equipment table
+INSERT INTO Equipment (equipment_id, equipment_name, equipment_condition) VALUES
+(1, 'Dumbell', 'Good'),
+(2, 'Yoga Mat', 'Good'),
+(3, 'Peloton Bike', 'Used'),
+(4, 'Treadmill', 'Broken');
